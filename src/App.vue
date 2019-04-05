bc00a6e2f2dbd420eee7c58d283a88438b0dabdb
c-imaizumi <c-imaizumi@mediba.jp>
2019-04-05 18:11:01 +0900

    いったんコミット

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,11 @@ import CardItems from './components/card_item';
 import CardKeep from './components/card_keep';
 
 function sleep(time) {
-  setTimeout(() => {
-
-  }, time);
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
 }
 
 export default {
@@ -27,40 +29,63 @@ export default {
   data() {
     return {
       cards: [
-        [{mark: "♡", number: 3, isFront: false, existence: true}, {mark: "♤", number: 10, isFront: false, existence: true}, {mark: "♧", number: 6, isFront: false, existence: true}],
-        [{mark: "♢", number: 5, isFront: false, existence: true}, {mark: "♧", number: 11, isFront: false, existence: true}, {mark: "♡", number: 12, isFront: false, existence: true}],
-        [{mark: "♢", number: 1, isFront: false, existence: true}, {mark: "♤", number: 6, isFront: false, existence: true}, {mark: "♡", number: 2, isFront: false, existence: true}]
+        [{mark: "♡", number: 1, isFront: false, existence: true}, {mark: "♡", number: 2, isFront: false, existence: true}, {mark: "♡", number: 3, isFront: false, existence: true}, {mark: "♡", number: 4, isFront: false, existence: true}, {mark: "♡", number: 5, isFront: false, existence: true}, {mark: "♡", number: 6, isFront: false, existence: true}, {mark: "♡", number: 7, isFront: false, existence: true}, {mark: "♡", number: 8, isFront: false, existence: true}, {mark: "♡", number: 9, isFront: false, existence: true}, {mark: "♡", number: 10, isFront: false, existence: true}, {mark: "♡", number: 11, isFront: false, existence: true}, {mark: "♡", number: 12, isFront: false, existence: true}, {mark: "♡", number: 13, isFront: false, existence: true}],
+        [{mark: "♢", number: 1, isFront: false, existence: true}, {mark: "♢", number: 2, isFront: false, existence: true}, {mark: "♢", number: 3, isFront: false, existence: true}, {mark: "♢", number: 4, isFront: false, existence: true}, {mark: "♢", number: 5, isFront: false, existence: true}, {mark: "♢", number: 6, isFront: false, existence: true}, {mark: "♢", number: 7, isFront: false, existence: true}, {mark: "♢", number: 8, isFront: false, existence: true}, {mark: "♢", number: 9, isFront: false, existence: true}, {mark: "♢", number: 10, isFront: false, existence: true}, {mark: "♢", number: 11, isFront: false, existence: true}, {mark: "♢", number: 12, isFront: false, existence: true}, {mark: "♢", number: 13, isFront: false, existence: true}],
+        [{mark: "♠︎", number: 1, isFront: false, existence: true}, {mark: "♠︎", number: 2, isFront: false, existence: true}, {mark: "♠︎", number: 3, isFront: false, existence: true}, {mark: "♠︎", number: 4, isFront: false, existence: true}, {mark: "♠︎", number: 5, isFront: false, existence: true}, {mark: "♠︎", number: 6, isFront: false, existence: true}, {mark: "♠︎", number: 7, isFront: false, existence: true}, {mark: "♠︎", number: 8, isFront: false, existence: true}, {mark: "♠︎", number: 9, isFront: false, existence: true}, {mark: "♠︎", number: 10, isFront: false, existence: true}, {mark: "♠︎", number: 11, isFront: false, existence: true}, {mark: "♠︎", number: 12, isFront: false, existence: true}, {mark: "♠︎", number: 13, isFront: false, existence: true}],
+        [{mark: "♧", number: 1, isFront: false, existence: true}, {mark: "♧", number: 2, isFront: false, existence: true}, {mark: "♧", number: 3, isFront: false, existence: true}, {mark: "♧", number: 4, isFront: false, existence: true}, {mark: "♧", number: 5, isFront: false, existence: true}, {mark: "♧", number: 6, isFront: false, existence: true}, {mark: "♧", number: 7, isFront: false, existence: true}, {mark: "♧", number: 8, isFront: false, existence: true}, {mark: "♧", number: 9, isFront: false, existence: true}, {mark: "♧", number: 10, isFront: false, existence: true}, {mark: "♧", number: 11, isFront: false, existence: true}, {mark: "♧", number: 12, isFront: false, existence: true}, {mark: "♧", number: 13, isFront: false, existence: true}]
       ],
       selectedCards: [],
     }
+  },
+  created() {
+    let array;
+    for(var item = 0; this.cards.length > item; item++) {
+      array = this.cards[item];
+
+      for(var l = array.lengtn -1; l > 0; l--) {
+        const m = Math.floor(Math.random() * (i +1));
+        [array[m], array[l]] = [array[l], array[m]]
+        return array;
+      }
+      console.log(array);
+    }
+    
+    const cardRow = [this.cards[0][0], this.cards[1][0], this.cards[2][0], this.cards[3][0]];
+    // TODO newAryをシャッフル
+    this.cards[0, 0] = cardRow[0, 0];
+    this.cards[1, 0] = cardRow[1, 0];
+    this.cards[2, 0] = cardRow[2, 0];
+    this.cards[3, 0] = cardRow[3, 0];
   },
   components: {
     CardItems,
   },
   methods: {
-    onClickCard(card) {
+    async onClickCard(card) {
       card.isFront = !card.isFront;
       this.selectedCards.push(card);
 
       if (this.selectedCards.length === 2) { // TODO カードの数字を比較
-        
         const firstCard = this.selectedCards[0];
         const secondCard = this.selectedCards[1];
-        if (firstCard.number === secondCard.number) { // 一緒だった時
+        
+        if (firstCard.number === secondCard.number) {
+          await sleep(1000);
           firstCard.existence = false;
           secondCard.existence = false;
           this.selectedCards = [];
-        }else { //外れた時 
-          setTimeout(stateChange, 1000);          
-          function stateChange() {
-            firstCard.isFront = false;
-            secondCard.isFront = false;
-          }
+        } else {
+          await sleep(1000);
+          firstCard.isFront = false;
+          secondCard.isFront = false;
           this.selectedCards = [];
         }
       }
     },
   },
+  mounted: {
+
+  }
 }
 </script>
 
